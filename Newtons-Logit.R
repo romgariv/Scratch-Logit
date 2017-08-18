07897f711b672bc07267338a98de3fe3a8b292bd
romgariv <romgariv@gmail.com>
2017-08-17 19:47:00 -0500

    updates

--- Newtons-Logit.R
+++ Newtons-Logit.R
@@ -19,12 +19,12 @@ GradientReg <- function(x,y,theta,l, reg.intercept) {
 
 HessianReg <- function(x,y,theta,l, reg.intercept) {
   return (
-    1/nrow(y) * ( (t(x) %*% diag(as.vector(sigmoid(X,theta) * (1 - sigmoid(X, theta)))) %*% x) 
+    1/nrow(y) * ( (t(x) %*% diag(as.vector(sigmoid(x,theta) * (1 - sigmoid(x, theta)))) %*% x) 
                   + l * diag(as.vector(rbind((1 - reg.intercept), matrix(1, nrow = (ncol(x) - 1), ncol = 1)))) )
   )
 }
 
-NMReg <-function(x,y,l,tolerance, reg.intercept) {
+NMReg <-function(x,y,l,tolerance, reg.intercept, print.LL = FALSE) {
   theta <- matrix(0, ncol = 1, nrow = ncol(x))
   LL.old <- 1000000000
   LL.new <- LLReg(x,y,theta,l, reg.intercept)
@@ -32,7 +32,7 @@ NMReg <-function(x,y,l,tolerance, reg.intercept) {
     LL.old <- LL.new
     theta <- theta - solve(HessianReg(x,y,theta,l, reg.intercept), GradientReg(x,y,theta,l,reg.intercept))
     LL.new <- LLReg(x,y,theta,l, reg.intercept)
-    print(LL.new)
+    if (print.LL) print(LL.new)
   }
   return (theta)
 }
@@ -67,21 +67,36 @@ Numeric.Standard.Errors <- function(theta, x, y, epp = 1e-6) {
   for (i in 1:nrow(theta)) {
     h[i] <- max(epp, d[i])
   }
-  Gi <- matrix(NA, nrow = nrow(X), ncol = ncol(X))
+  
+  Gi <- matrix(NA, nrow = nrow(x), ncol = ncol(x))
   
   for (i in 1:nrow(theta)) {
     t1 <- theta
     t2 <- theta
     t1[i,] <- theta[i,] + h[i]
     t2[i,] <- theta[i,] - h[i]
-    Gi[,i] <- (LLReg(X,Y,t1,0,0,FALSE) - LLReg(X,Y,t2,0,0,FALSE))/(2*h[i])
+    Gi[,i] <- (LLReg(x,y,t1,0,0,FALSE) - LLReg(x,y,t2,0,0,FALSE))/(2*h[i])
   }
   return( sqrt(diag(solve(t(Gi) %*% Gi))) )
 }
 
-# T.Statistics <- function(theta, SE) {
-#   return(theta/SE)
-# }
+Z.Score <- function(theta, SE) {
+  return(theta/SE)
+}
 
-#P.Values <- function
+P.Value <- function(z) {
+  return (1.96 * pnorm(-abs(z)))
+}
 
+Logit.Summary <- function(x,y) {
+  theta <- NMLogit(x, y)
+  se <- Standard.Errors(theta, x)
+  z <- Z.Score(theta, se)
+  p <- P.Value(z)
+  df <- data.frame(Estimates = round(theta,8),
+                   `Std. Error` = as.vector(round(se,7)),
+                   `Z-Score` = as.vector(round(z,3)),
+                   `P-Value` = as.vector(round(p,4)))
+  colnames(df) <- c("Estimates", "Std. Error", "Z-Score", "P-Value")
+  return(df)
+}
